7af11ada82b983da446299d95a5c0c52b91fda4b
Laercio Rios <laercio.rios13@hotmail.com>
2023-04-11 10:11:47 -0300

    :wrench: Add documentation's configuration

---
+++ src/config/swagger.ts
@@ -0,0 +1,10 @@
+const swaggerOptions = {
+  customCss: `
+    .swagger-ui .topbar { 
+      display: none
+    }
+  `,
+  customSiteTitle: "API Broadcasts Documentation",
+  // customfavIcon: "",
+};
+export default swaggerOptions;
--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -2,15 +2,26 @@ import "reflect-metadata";
 import "dotenv/config";
 import cors from "cors";
 import express from "express";
+import swaggerUi from "swagger-ui-express";
 
 import { router } from "./routes";
 
 import "@shared/container";
+
+import swaggerFile from "../../../swagger.json";
+
+import swaggerOptions from "@config/swagger";
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 app.use(router);
+
+app.use(
+  "/api/docs",
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerFile, swaggerOptions)
+);
 
 export { app };
