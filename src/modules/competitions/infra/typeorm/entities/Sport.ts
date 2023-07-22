bd4e4c16645510659cc50d24d0a85196db40f302
Laercio Rios <laercio.rios13@hotmail.com>
2023-07-22 15:47:04 -0300

    :bento: Add sport icons

---
+++ src/modules/competitions/infra/typeorm/entities/Sport.ts
@@ -0,0 +1,38 @@
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryColumn,
+  UpdateDateColumn,
+} from "typeorm";
+import { v4 as uuidV4 } from "uuid";
+
+@Entity("sport")
+export class Sport {
+  @PrimaryColumn()
+  id!: string;
+
+  @Column()
+  name!: string;
+
+  @Column()
+  logo?: string;
+
+  @CreateDateColumn({
+    default: () => "CURRENT_TIMESTAMP",
+  })
+  created_at!: Date;
+
+  @UpdateDateColumn({ select: false })
+  updated_at?: Date;
+
+  @DeleteDateColumn({ select: false })
+  deleted_at?: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuidV4();
+    }
+  }
+}
